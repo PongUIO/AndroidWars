ef4371c5b14f4b3f573b3657701ea3a40c672104
TZer0 <TZer0.jan@gmail.com>
2011-06-02 22:07:00 +0200

    Opengl in background, one button on top of it!

--- pc/main.cpp
+++ pc/main.cpp
@@ -1,23 +1,24 @@
 #include <QtGui>
 #include <QWidget>
 #include "main.h"
-#include "test.h"
+#include "ui.h"
+#include "../openglqt/render.h"
 
 int main(int argc, char *argv[]) {
 	QApplication app(argc, argv);
-	GameUi *window;
-	window = new GameUi(&app);
-	window->setWindowTitle(QApplication::translate("childwidget", "Child widget"));
-	window->showFullScreen();
-	//window->show();
-
+	const int width = QApplication::desktop()->width();
+	const int height = QApplication::desktop()->height();
+	MyGLDrawer *drawer = new MyGLDrawer(0);
+	drawer->showFullScreen();
+	drawer->resize(width, height);
+	drawer->setWindowTitle(QApplication::translate("childwidget", "Child widget"));
 	QPushButton *button = new QPushButton(
-			QApplication::translate("childwidget", "Press me"), window);
+			QApplication::translate("childwidget", "Press me"), drawer);
 	button->move(100, 100);
 
 	button->show();
 	//clicked();
-	window->connect(button, SIGNAL(clicked()), window, SLOT(c()));
+	//window->connect(button, SIGNAL(clicked()), window, SLOT(c()));
 	return app.exec();
 }
 
--- pc/test.h
+++
@@ -1,7 +0,0 @@
-#include<QtGui>
-#include "ui_test.h"
-class GameUi : public QMainWindow, private Ui::MainWindow {
-	Q_OBJECT
-	public:
-		GameUi(QApplication *app, QWidget *parent=0);
-};
