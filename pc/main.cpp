2f4dd1d778cf5e7be66bcd360b43ed7cd6103d3d
TZer0 <TZer0.jan@gmail.com>
2011-06-09 22:46:52 +0200

    QAbstractButton!

--- pc/main.cpp
+++ pc/main.cpp
@@ -1,22 +1,24 @@
 #include <QtGui>
 #include <QWidget>
+#include <QAbstractButton>
+#include <QIcon>
 #include "main.h"
 #include "ui.h"
 #include "../openglqt/render.h"
 
 int main(int argc, char *argv[]) {
 	QApplication app(argc, argv);
-	const int width = QApplication::desktop()->width();
-	const int height = QApplication::desktop()->height();
 	MyGLDrawer *drawer = new MyGLDrawer(0);
 	drawer->showFullScreen();
-	drawer->resize(width, height);
 	drawer->setWindowTitle(QApplication::translate("childwidget", "Child widget"));
 	drawer->setMouseTracking(true);
-	QPushButton *button = new QPushButton(
-			QApplication::translate("childwidget", "Press me"), drawer);
+	QIcon icon("temp.png");
+	QAbstractButton *button = new QPushButton(drawer);
+//			QApplication::translate("childwidget", "Press me"), drawer);
+//	button->setStyleSheet("* { image: url(temp.png);}");
+	button->setIcon(icon);
 	button->move(100, 100);
-
+//	button->setFlat(true);
 	button->show();
 	//clicked();
 	//window->connect(button, SIGNAL(clicked()), window, SLOT(c()));
