5a729bebfb014a8113ef47eb2014b6a99454f2f8
TZer0 <tzer0.jan@gmail.com>
2012-07-05 21:06:29 +0200

    update: README, setupenvironment, developing skeletalsystem.h

--- openglqt/test/SkeletalSystem/main.cpp
+++ openglqt/test/SkeletalSystem/main.cpp
@@ -9,6 +9,11 @@ int main(int argc, char *argv[])
 	qDebug() << b;
 	int b2 = s.addBoneToSkeleton(skel, QVector3D(5,4,3), b);
 	qDebug() << b2;
-	s.rotateBone(0, 0, QMatrix4x4(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1));
-
+	s.rotateBone(0, 1, QMatrix4x4(0,2,0,0,2,0,0,0,0,0,2,0,0,0,0,2));
+	s.rotateBone(0, 0, QMatrix4x4(2,0,0,0,0,2,0,0,0,0,2,0,0,0,0,2));
+	s.calculateRotations();
+	qDebug() << s.skeletons.at(0)->bones.at(0)->rot;
+	qDebug() << s.skeletons.at(0)->bones.at(0)->rotOrigin;
+	qDebug() << s.skeletons.at(0)->bones.at(1)->rot;
+	qDebug() << s.skeletons.at(0)->bones.at(1)->rotOrigin;
 }
