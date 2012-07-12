b58a70d6d8dd2e8284888775dec5244b1d2b40a6
TZer0 <tzer0.jan@gmail.com>
2012-07-12 21:04:55 +0200

    WIP skeletal system.

--- openglqt/skeletalsystem.h
+++ openglqt/skeletalsystem.h
@@ -1,37 +1,32 @@
 #ifndef SKELETALSYSTEM_H
 #define SKELETALSYSTEM_H
-#include <QMatrix4x4>
-#include <QVector3D>
+#include <QDebug>
 #include <QList>
+#include <QQuaternion>
+#include <QMatrix4x4>
+
 class Bone {
 	public:
 		Bone *previous;
-		QMatrix4x4 rot;
-		QVector3D origin, rotOrigin;
-		Bone(Bone *p, QVector3D o) {
+		QQuaternion origin, rotOrigin, rot;
+		Bone(Bone *p, QQuaternion o) {
 			previous = p;
-			origin = rotOrigin = o;
-			rot.setToIdentity();
+			origin = rotOrigin = QQuaternion();
+			rot = o;
 		}
 		void calcMatrix() {
 			rotOrigin = origin;
 			Bone *tmp = previous;
-			QList<Bone*> order;
-			while (tmp != NULL) {
-				order.push_back(tmp);
-				tmp = tmp->previous;
-			}
-			while (order.size() != 0) {
-				tmp = order.last();
-				order.pop_back();
+			if (tmp != NULL) {
 				rot *= tmp->rot;
 				rotOrigin = tmp->rot * rotOrigin;
+				qDebug() << rotOrigin;
 				qDebug() << "debug";
 				qDebug() << tmp->rot * rotOrigin;
 			}
 		}
 
-		void setRotation(QMatrix4x4 in) {
+		void setRotation(QQuaternion in) {
 			rot = in;
 		}
 };
@@ -40,7 +35,7 @@ class Skeleton {
 	public:
 		QList<Bone*> bones;
 		Skeleton() {}
-		int addBone(QVector3D boneOrigin, int pos) {
+		int addBone(QQuaternion boneOrigin, int pos) {
 			if (pos < 0) {
 				bones.push_back(new Bone(NULL, boneOrigin));
 			} else {
@@ -48,7 +43,7 @@ class Skeleton {
 			}
 			return bones.size()-1;
 		}
-		void rotateBone(int bone, QMatrix4x4 rot) {
+		void rotateBone(int bone, QQuaternion rot) {
 			bones.at(bone)->rot = rot;
 		}
 		void calculateRotations() {
@@ -65,16 +60,21 @@ class SkeletalSystem {
 			skeletons.push_back(new Skeleton());
 			return skeletons.size()-1;
 		}
-		int addBoneToSkeleton(int s, QVector3D origin, int bone) {
+		int addBoneToSkeleton(int s, QQuaternion origin, int bone) {
 			return skeletons.at(s)->addBone(origin, bone);
 		}
-		void rotateBone(int s, int b, QMatrix4x4 rot) {
+		void rotateBone(int s, int b, QQuaternion rot) {
 			skeletons.at(s)->rotateBone(b, rot);
 		}
 		void calculateRotations() {
 			for (int i = 0; i < skeletons.size(); i++) {
 				skeletons.at(i)->calculateRotations();
 			}
+		}
+		QMatrix4x4 getMatrixForSkeleton(int s, int b) {
+			QMatrix4x4 ret;
+			ret.rotate(skeletons.at(s)->bones.at(b)->rot);
+			return ret;
 		}
 };
 
--- openglqt/test/SkeletalSystem/main.cpp
+++ openglqt/test/SkeletalSystem/main.cpp
@@ -4,16 +4,19 @@ int main(int argc, char *argv[])
 {
 	SkeletalSystem s;
 	int skel = s.addSkeleton();
-	qDebug() << skel;
-	int b = s.addBoneToSkeleton(skel, QVector3D(3,2,1), -1);
+	int b = s.addBoneToSkeleton(skel, QQuaternion(1,0,-1,1), -1);
 	qDebug() << b;
-	int b2 = s.addBoneToSkeleton(skel, QVector3D(5,4,3), b);
-	qDebug() << b2;
-	s.rotateBone(0, 1, QMatrix4x4(0,2,0,0,2,0,0,0,0,0,2,0,0,0,0,2));
-	s.rotateBone(0, 0, QMatrix4x4(2,0,0,0,0,2,0,0,0,0,2,0,0,0,0,2));
-	s.calculateRotations();
-	qDebug() << s.skeletons.at(0)->bones.at(0)->rot;
+	int b2 = s.addBoneToSkeleton(skel, QQuaternion(1,0,0,1), b);
+
+	qDebug() << s.getMatrixForSkeleton(0, 0);
 	qDebug() << s.skeletons.at(0)->bones.at(0)->rotOrigin;
-	qDebug() << s.skeletons.at(0)->bones.at(1)->rot;
+	qDebug() << s.getMatrixForSkeleton(0, 1);
+	qDebug() << s.skeletons.at(0)->bones.at(1)->rotOrigin;
+	//s.rotateBone(0, 1, QQuaternion(1,0,0,1));
+	//s.rotateBone(0, 0, QQuaternion(2,0,2,0));
+	s.calculateRotations();
+	qDebug() << s.getMatrixForSkeleton(0, 0);
+	qDebug() << s.skeletons.at(0)->bones.at(0)->rotOrigin;
+	qDebug() << s.getMatrixForSkeleton(0, 1);
 	qDebug() << s.skeletons.at(0)->bones.at(1)->rotOrigin;
 }
